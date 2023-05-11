a1b2f08a751ca2df5154fbfae4506f37d7734ca4
bhumi80 <sutharbb80@gmail.com>
2023-05-12 00:29:30 +0100

    commit

---
+++ src/test/java/org/example/FindElements.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+import java.util.Set;
+
+public class FindElements {
+    public WebDriver driver;
+    public String url = "https://www.letskodeit.com/practice";
+    @Before
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(url);
+    }
+    @Test
+    public void getElements(){
+       List<WebElement> labelElements = driver.findElements(By.xpath("//div[@id='radio-btn-example']/fieldset/label"));
+
+        for (WebElement labelElement:labelElements) {
+            System.out.println(labelElement.getText());
+            
+        }
+    }
+    @After
+    public void tearDown(){
+        driver.close();
+    }
+}
